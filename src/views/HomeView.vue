b978b30ad45b60a1d6a1136efd63bcc99e6df18f
Giandonn <lucas_raineri04@hotmail.com>
2024-04-16 20:08:46 -0300

    upt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,13 @@
     <div class="container">
       <div class="row mt-4">
         <div class="col-sm-12 col-md-6">
-          Oi
+          <div class="card">
+            <img src="" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card Title</h5>
+              <p class="card-text">Um texto resumido e pequeno</p>
+            </div>
+          </div>
         </div>
         <div class="col-sm-12 col-md-6">
           Ola
