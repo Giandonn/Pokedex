9701b7d7ace7ee0e6e9093625cb08033ca91b0f3
Giandonn <lucas_raineri04@hotmail.com>
2024-04-16 20:08:11 -0300

    teste

---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Esse é um teste Unimar!</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <div class="row mt-4">
+        <div class="col-sm-12 col-md-6">
+          Oi
+        </div>
+        <div class="col-sm-12 col-md-6">
+          Ola
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
